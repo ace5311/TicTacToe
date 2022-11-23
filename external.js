d8a1b6c4dffb914b012906060d274f6ccb792513
ace5311 <erneandrew@gmail.com>
2022-11-22 18:35:56 -0800

    Reset resets prompt

--- external.js
+++ external.js
@@ -57,7 +57,7 @@ const gameboard = (() => {
         turns=0;
         chosen.player1 = [];
         chosen.player2 = [];
-        phrase.textContent=`Let's Play`
+        phrase.textContent=`Let's Play!`
     }
 
     const playerTurn = () => {
